82a07afa12eca4abf5c3acf65a6bca4df6d73782
Agustín <amfc@afdez.no-ip.org>
2008-08-17 02:17:40 -0300

    added tinkerman

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<title>mini.adondevamos.com</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>cines o películas</title>
 <link type="text/css" rel="StyleSheet" href="style.css">
 <script type="text/javascript" src="/js/dom.js"></script>
 <script type="text/javascript" src="/js/variables.js"></script>
@@ -10,6 +10,10 @@
 <script type="text/javascript" src="navigation.js"></script>
 <script type="text/javascript" src="template.js"></script>
 <script type="text/javascript" src="main.js"></script>
+<?
+$tinkermanPath = 'tinkerman/';
+include('tinkerman/includeLog.php');
+?>
 </head>
 <body onload="app.load()">
 <iframe id="NavigationIframe" src="navigation.html" style="display: none"></iframe>
